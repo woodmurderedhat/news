fb33366c8c24601de9afaf69db5f6c1935abf7df
woodmurderedhat <imurderedthatunicorn@gmail.com>
2025-01-31 20:26:31 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,16 @@ document.addEventListener('DOMContentLoaded', () => {
     const articlesContainer = document.getElementById('articles-container');
 
     fetch('articles')
-        .then(response => response.json())
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            const contentType = response.headers.get('content-type');
+            if (!contentType || !contentType.includes('application/json')) {
+                throw new TypeError("Expected JSON, but got " + contentType);
+            }
+            return response.json();
+        })
         .then(articles => {
             articles.sort((a, b) => new Date(b.date) - new Date(a.date));
             articles.forEach(article => {
